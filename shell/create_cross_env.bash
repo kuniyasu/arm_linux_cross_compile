84a6dcc07ada305097302a48056a8f3e6e6a0ab4
kuniyasu <kuniyasu@localhost.localdomain>
2015-08-19 06:46:36 +0900

    Modified script
    GCC cross compiler code is added. Binutil compile option is modified.
    libstdc++ library install script is added.

--- shell/create_cross_env.bash
+++ shell/create_cross_env.bash
@@ -18,7 +18,9 @@ SYSROOT=$PWD
 PREFIX=$SYSROOT
 
 PATH=$PREFIX/bin:$PATH
-LD_LIBRARY_PATH=$PREFIX/lib64:$PREFIX/lib:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=$PREFIX/lib64:$PREFIX/lib:$PREFIX/$TARGET/lib:$LD_LIBRARY_PATH
+
+FP_OPT="--with-fp --with-float=hard --with-fpu=neon-vfpv4 --with-mode=thumb --with-arch=armv7-a "
 
 function download_file(){
     if [ ! -f $2 ]; then
@@ -56,7 +58,8 @@ function install_binutils(){
     cd build-$BINUTILS_NAME
       ../$BINUTILS_NAME/configure \
       --prefix=$PREFIX --target=$TARGET \
-      --disable-multilib
+      --with-sysroot=$PREFIX/$TARGET
+
 
       make $MAKE_OPT
 
@@ -73,6 +76,8 @@ function install_binutils(){
       ln -s usr/bin bin
       ln -s usr/lib lib
       ln -s usr/include include
+      ln -s usr/sbin sbin
+
     fi
 
   popd
@@ -115,7 +120,13 @@ function install_bootstrap_gcc(){
       --disable-libssp \
       --without-headers \
       --enable-languages=c \
-      --disable-multilib
+      --disable-multilib \
+      --with-fp \
+      --with-float=hard \
+      --with-fpu=neon-vfpv4 \
+      --with-mode=thumb \
+      --with-arch=armv7-a
+
 
       make $MAKE_OPT all-gcc
       make $MAKE_OPT all-target-libgcc
@@ -140,12 +151,12 @@ install_bootstrap_linux(){
     mv $LINUX_NAME build0-$LINUX_NAME
 
     cd build0-$LINUX_NAME
+      make mrproper
 
       make ARCH=arm $LINUX_DEFCONFIG
       make ARCH=arm headers_check
 
       make ARCH=arm \
-      CROSS_COMPILE=$TARGET- \
       INSTALL_HDR_PATH=$PREFIX/$TARGET/usr \
       headers_install \
 
@@ -180,12 +191,18 @@ install_bootstrap_glibc(){
       --host=$TARGET \
       --target=$TARGET \
       libc_cv_forced_unwind=yes \
-      libc_cv_ctors_header=yes \
       libc_cv_c_cleanup=yes \
       --prefix=/usr \
       --with-headers=$PREFIX/$TARGET/usr/include \
       --disable-multilib \
+      --with-fp \
+      --with-float=hard \
+      --with-fpu=neon-vfpv4 \
+      --with-mode=thumb \
+      --with-arch=armv7-a \
+      --disable-sjlj-exceptions \
       --enable-threads=posix \
+      --disable-nscd \
       --config-cache
 
       make $MAKE_OPT
@@ -194,13 +211,231 @@ install_bootstrap_glibc(){
   cd ..
 
   if [ -d $PREFIX/$TARGET/usr/sys-include ]; then
-    rm -rf $PREFIX/$TARGET/usr/sys-include
+    rm -rf $PREFIX/$TARGET/sys-include
   fi
 
 }
 
+install_gcc_build1(){
+  if [ ! -d build ]; then
+    mkdir build
+  fi
+
+  cd build
+
+  if [ -d $GCC_NAME ]; then
+    rm -rf $GCC_NAME
+  fi
+
+  tar zxf ../download/$GCC_NAME.tar.gz
+
+  cd $GCC_NAME
+    ./contrib/download_prerequisites
+  cd ..
+
+  if [ -d build1-$GCC_NAME ]; then
+    rm -rf build1-$GCC_NAME
+  fi
+
+  mkdir build1-$GCC_NAME
+
+    cd build1-$GCC_NAME
+
+      ../$GCC_NAME/configure --prefix=$PREFIX --target=$TARGET \
+      --with-headers=$PREFIX/$TARGET/usr/include \
+      --enable-languages=c \
+      --disable-multilib \
+      --with-fp \
+      --with-float=hard \
+      --with-fpu=neon-vfpv4 \
+      --with-mode=thumb \
+      --with-arch=armv7-a
+
+      make $MAKE_OPT
+      make install
+
+    cd ..
+  cd ..
+
+  if [ -d $PREFIX/$TARGET/sys-include ]; then
+    rm -rf $PREFIX/$TARGET/sys-include
+  fi
+
+}
+
+install_linux_kernel_header(){
+  if [ ! -d build ]; then
+    mkdir build
+  fi
+
+  cd build
+
+  if [ -d build1-$LINUX_NAME ]; then
+    rm -rf build1-$LINUX_NAME
+  fi
+
+  tar zxf ../download/$LINUX_NAME.tar.gz
+
+  mv $LINUX_NAME build1-$LINUX_NAME
+
+  cd build1-$LINUX_NAME
+    make mrproper
+    make ARCH=arm $LINUX_DEFCONFIG
+    make ARCH=arm headers_check
+    make ARCH=arm INSTALL_HDR_PATH=$PREFIX headers_install
+  cd ..
+
+  cd ..
+}
+
+install_glibc(){
+  if [ ! -d build ]; then
+    mkdir build
+  fi
+
+  cd build
+
+  if [ -d $GLIBC_NAME ]; then
+    rm -rf $GLIBC_NAME
+  fi
+
+  tar zxf ../download/$GLIBC_NAME.tar.gz
+
+  if [ -d build1-$GLIBC_NAME ]; then
+    rm -rf build1-$GLIBC_NAME
+  fi
+
+  mkdir build1-$GLIBC_NAME
+
+  cd build1-$GLIBC_NAME
+
+    CC=$TARGET-gcc \
+    CXX=$TARGET-g++ \
+    LD=$TARGET-gcc \
+    AS=$TARGET-as \
+    RANLIB=$TARGET-ranlib \
+    ../$GLIBC_NAME/configure \
+    libc_cv_forced_unwind=yes \
+    libc_cv_c_cleanup=yes \
+    --disable-werror \
+    --build=x86_64-unknown-linux-gnu \
+    --host=$TARGET \
+    --target=$TARGET \
+    --prefix=/ \
+    --disable-multilib \
+    --with-headers=$PREFIX/include \
+    --with-fp \
+    --with-float=hard \
+    --with-fpu=neon-vfpv4 \
+    --with-mode=thumb \
+    --with-arch=armv7-a \
+    --disable-sjlj-exceptions \
+    --enable-threads=posix \
+    --disable-nscd \
+    --config-cache
+
+    make $MAKE_OPT
+    make install install_root=$SYSROOT
+  cd ..
+  cd ..
+
+  if [ -d $PREFIX/$TARGET/sys-include ]; then
+    rm -rf $PREFIX/$TARGET/sys-include
+  fi
+}
+
+install_gcc_build2(){
+  if [ ! -d build ]; then
+    mkdir build
+  fi
+
+  cd build
+
+  if [ -d $GCC_NAME ]; then
+    rm -rf $GCC_NAME
+  fi
+
+  tar zxf ../download/$GCC_NAME.tar.gz
+
+  cd $GCC_NAME
+    ./contrib/download_prerequisites
+  cd ..
+
+  if [ -d build2-$GCC_NAME ]; then
+    rm -rf build2-$GCC_NAME
+  fi
+
+  mkdir build2-$GCC_NAME
+
+  cd build2-$GCC_NAME
+
+  ../$GCC_NAME/configure --prefix=$PREFIX --target=$TARGET \
+  --with-headers=$PREFIX/include \
+  --disable-multilib \
+  --with-fp \
+  --with-float=hard \
+  --with-fpu=neon-vfpv4 \
+  --with-mode=thumb \
+  --with-arch=armv7-a
+
+  make $MAKE_OPT
+  make install
+
+  cd ..
+  cd ..
+}
+
+install_libstdcxx(){
+  if [ ! -d build ]; then
+    mkdir build
+  fi
+
+  cd build
+
+  if [ -d $GCC_NAME ]; then
+    rm -rf $GCC_NAME
+  fi
+
+  tar zxf ../download/$GCC_NAME.tar.gz
+  cd $GCC_NAME
+    ./contrib/download_prerequisites
+  cd ..
+
+  if [ -d build_libstdxx-$GCC_NAME ]; then
+    rm -rf build_libstdxx-$GCC_NAME
+  fi
+
+  mkdir build_libstdxx-$GCC_NAME
+
+  cd build_libstdxx-$GCC_NAME
+
+  CC=$TARGET-gcc \
+  CXX=$TARGET-g++ \
+  LD=$TARGET-gcc \
+  AS=$TARGET-as \
+  RANLIB=$TARGET-ranlib \
+  ../$GCC_NAME/libstdc++-v3/configure --prefix=$PREFIX --host=$TARGET \
+  --with-gxx-include-dir=$PREFIX/include/c++/$GCC_VER \
+  --disable-multilib \
+  --disable-libstdcxx-pch
+
+  make
+  make install
+  cd ..
+  cd ..
+}
+
 download_files
 install_binutils
+
 install_bootstrap_gcc
 install_bootstrap_linux
 install_bootstrap_glibc
+
+install_gcc_build1
+
+install_linux_kernel_header
+install_glibc
+install_gcc_build2
+
+install_libstdcxx
