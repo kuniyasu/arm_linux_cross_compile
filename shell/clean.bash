f925298639ef163324c041cfae63346c6a441f09
Kuniyasu <kuniyasu@basil.ocn.ne.jp>
2015-08-18 16:51:51 +0900

    Modified shell
    ADD bootstrap-gcc compilation.

--- shell/clean.bash
+++ shell/clean.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for a in build bin lib lib64 include etc share arm-unknown-linux-gnueabihf; do
+for a in build bin lib lib64 libexec build include etc share arm-unknown-linux-gnueabihf; do
   if [ -d $a ]; then
     rm -rf $a
   fi
--- shell/create_cross_env.bash
+++ shell/create_cross_env.bash
@@ -5,7 +5,7 @@ MAKE_OPT=-j8
 
 BINUTILS_VER=2.25
 GCC_VER=5.2.0
-LINUX_VER=4.2.0
+LINUX_VER=4.1.6
 GLIBC_VER=2.20
 
 BINUTILS_NAME=binutils-$BINUTILS_VER
@@ -20,7 +20,7 @@ PATH=$PREFIX/bin:$PATH
 LD_LIBRARY_PATH=$PREFIX/lib64:$PREFIX/lib:$LD_LIBRARY_PATH
 
 function download_file(){
-    if [ ! -f filename ]; then
+    if [ ! -f $2 ]; then
       wget $1/$2
     fi
 }
@@ -31,8 +31,10 @@ function download_files(){
   fi
   pushd download
 
-  download_file http://ftp.gnu.org/gnu/binutils/ $BINUTILS_NAME.tar.gz
-  download_file http://ftp.gnu.org/gnu/gcc/$GCC_NAME $GCC_NAME.tar.gz
+    download_file http://ftp.gnu.org/gnu/binutils/ $BINUTILS_NAME.tar.gz
+    download_file http://ftp.gnu.org/gnu/gcc/$GCC_NAME $GCC_NAME.tar.gz
+    download_file https://www.kernel.org/pub/linux/kernel/v4.x/ $LINUX_NAME.tar.gz
+    download_file http://ftp.gnu.org/gnu/glibc/ $GLIBC_NAME.tar.gz
 
   popd
 
@@ -62,13 +64,69 @@ function install_binutils(){
   cd ..
 
   pushd $TARGET
-    mkdir usr
-    mv bin lib usr
-    ln -s usr/bin bin
-    ln -s usr/lib lib
-    ln -s usr/include include
+    if [ ! -d usr ]; then
+      mkdir usr
+
+      mv bin lib usr
+
+      ln -s usr/bin bin
+      ln -s usr/lib lib
+      ln -s usr/include include
+    fi
+
   popd
+}
+
+function install_bootstrap(){
+  if [ ! -d build ]; then
+    mkdir build
+  fi
+
+  if [ ! -d build/build0-$GCC_NAME ]; then
+    mkdir build/build0-$GCC_NAME
+  fi
+
+  cd build
+
+    tar zxf ../download/$GCC_NAME.tar.gz
+
+    cd $GCC_NAME
+      ./contrib/download_prerequisites
+    cd ..
+
+    cd build0-$GCC_NAME
+
+      ../$GCC_NAME/configure --prefix=$PREFIX --target=$TARGET \
+      --disable-threads \
+      --disable-shared \
+      --enable-__cxa_atexit \
+      --disable-libmudflap \
+      --disable-libgomp \
+      --disable-nls \
+      --disable-libstdcxx \
+      --disable-libatomic \
+      --disable-libquadmath \
+      --disable-decimal-float \
+      --disable-libitm \
+      --disable-libsanitizer \
+      --disable-libcilkrts \
+      --disable-cloog \
+      --disable-libssp \
+      --without-headers \
+      --enable-languages=c \
+      --disable-multilib
+
+      make $MAKE_OPT all-gcc
+      make $MAKE_OPT all-target-libgcc
+
+      make install-gcc
+      make install-target-libgcc
+
+    cd ..
+
+  cd ..
 }
 
 download_files
 install_binutils
+install_bootstrap
